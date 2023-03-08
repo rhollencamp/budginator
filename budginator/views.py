a71b32db3a5330bd4fa452af9f37d2164ee5db2d
Robert Hollencamp <rhollencamp@cargurus.com>
2023-03-08 13:49:11 -0500

    checkpoint

--- budginator/views.py
+++ budginator/views.py
@@ -1,10 +1,48 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpRequest
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods, require_GET
 
-from .models import Budget
+from .models import Budget, TrackedTransaction, TrackedTransactionSplit
 
-def index(request):
+
+@require_GET
+def index(request: HttpRequest):
     context = {
         'budgets': Budget.objects.order_by('name')
     }
     return render(request, 'budginator/index.html', context)
+
+
+@require_GET
+def list_transactions(request: HttpRequest):
+    context = {
+        'transactions': TrackedTransaction.objects.order_by('-date')
+    }
+    return render(request, 'budginator/transactions.html', context)
+
+
+@csrf_exempt # TODO fix csrf
+@require_http_methods(['GET', 'POST'])
+def track(request: HttpRequest):
+    if request.method == 'GET':
+        context = {
+            'budgets': Budget.objects.order_by('name')
+        }
+        return render(request, 'budginator/track.html', context)
+    else:
+        budget = get_object_or_404(Budget, pk=request.POST['budget'])
+        transaction = TrackedTransaction.objects.create(
+            amount=int(request.POST['amount']) * int(request.POST['multiplier']),
+            date=request.POST['date'],
+            merchant=request.POST['merchant']
+        )
+
+        TrackedTransactionSplit.objects.create(
+            amount=int(request.POST['amount']) * int(request.POST['multiplier']),
+            budget=budget,
+            note=request.POST['note'],
+            transaction=transaction
+        )
+
+        return HttpResponseRedirect('/transactions?budget=' + budget.name)
