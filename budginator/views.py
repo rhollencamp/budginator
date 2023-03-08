0356350c07ad2cc4ff33988b56d5c3529709cf3c
Robert Hollencamp <rhollencamp@cargurus.com>
2023-03-08 11:58:07 -0500

    checkpoint

--- budginator/views.py
+++ budginator/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
+from .models import Budget
 
 def index(request):
-    context = {}
+    context = {
+        'budgets': Budget.objects.order_by('name')
+    }
     return render(request, 'budginator/index.html', context)
