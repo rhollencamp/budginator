decd1141944599a9f8330d922905242aac22ae75
Robert Hollencamp <rhollencamp@gmail.com>
2023-03-10 16:51:09 -0500

    consider tracked transactions

--- budginator/tests.py
+++ budginator/tests.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 from django.test import TestCase
 
-from .models import Budget
+from .models import Budget, TrackedTransaction, TrackedTransactionSplit
 from .service import calculate_budgets_available, calculate_num_months, parse_amount
 
 
@@ -22,12 +22,27 @@ class ServiceTests(TestCase):
     def test_calculate_budgets_available(self):
         # create a budget starting last month
         start_date = date.today().replace(day=1) - timedelta(days=1)
-        Budget.objects.create(
+        budget = Budget.objects.create(
             amount=30000,
             icon='X',
             name='Test',
             start_date=start_date
         )
 
+        # we have no transactions, so should have two months of budget available
         budgets_available = calculate_budgets_available()
         self.assertEqual(60000, budgets_available['Test'])
+
+        # track something and make sure it is reflected
+        transaction = TrackedTransaction.objects.create(
+            amount=5000,
+            date=date.today(),
+            merchant='Test'
+        )
+        TrackedTransactionSplit.objects.create(
+            amount=5000,
+            budget=budget,
+            transaction=transaction
+        )
+        budgets_available = calculate_budgets_available()
+        self.assertEqual(55000, budgets_available['Test'])
