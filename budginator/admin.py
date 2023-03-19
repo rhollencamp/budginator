225fef7b100edf9fa72e1673afd7991168f20869
Robert Hollencamp <rhollencamp@gmail.com>
2023-03-19 17:22:46 -0400

    admin site show amount for budget

--- budginator/admin.py
+++ budginator/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 from .models import BankAccount, Budget
 
-admin.site.register([BankAccount, Budget])
+admin.site.register(BankAccount, list_display=['name'], ordering=['name'])
+admin.site.register(Budget, list_display=['name', 'amount'], ordering=['name'])
