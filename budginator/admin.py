47a36ccfd376f00e57e21e6a479e0b1fb3192d9b
Robert Hollencamp <rhollencamp@gmail.com>
2023-09-23 11:37:00 -0400

    tracked transactions in admin

--- budginator/admin.py
+++ budginator/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import BankAccount, Budget, ImportedTransaction
+from .models import BankAccount, Budget, ImportedTransaction, TrackedTransaction
 
 admin.site.register(BankAccount, list_display=['name'], ordering=['name'])
 admin.site.register(Budget, list_display=['name', 'amount'], ordering=['name'])
 admin.site.register(ImportedTransaction,
                     list_display=['date', 'merchant', 'amount'],
                     ordering=['date'])
+admin.site.register(TrackedTransaction, list_display=['date', 'merchant'], ordering=['date'])
