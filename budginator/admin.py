1f1e7cf094bb5eed57f80bc6ba2d4aff6627ef7a
Robert Hollencamp <rhollencamp@gmail.com>
2023-09-23 11:24:21 -0400

    show imported transactions in admin

--- budginator/admin.py
+++ budginator/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import BankAccount, Budget
+from .models import BankAccount, Budget, ImportedTransaction
 
 admin.site.register(BankAccount, list_display=['name'], ordering=['name'])
 admin.site.register(Budget, list_display=['name', 'amount'], ordering=['name'])
+admin.site.register(ImportedTransaction,
+                    list_display=['date', 'merchant', 'amount'],
+                    ordering=['date'])
